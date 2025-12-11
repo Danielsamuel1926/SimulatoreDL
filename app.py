1d63443f610e7934d42c03d9ff3e7c066e83d02b
Daniele Lettera <infodanielplay@gmail.com>
2025-12-11 23:03:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,6 @@ body {
 
 /* TABLE */
 .stTable td, .stTable th {
-    background-color: #009ACD;
     padding: 8px;
 }
 </style>
@@ -70,7 +69,7 @@ tipo = option_menu(
     default_index=0,
     orientation="horizontal",
     styles={
-        "container": {"background-color": "#D0EBFF"},
+        "container": {"background-color": "#a7a7a8"},
         "nav-link": {"font-size": "18px", "color": "#000"},
         "nav-link:hover": {"background-color": "#ADE8FF"},
         "nav-link-selected": {"background-color": "#00BFFF", "color": "white"},
@@ -207,6 +206,7 @@ if st.button("Calcola Bolletta", key="calc"):
 
     except Exception as e:
         st.error(f"Errore nel calcolo: {e}")
+
 
 
 
