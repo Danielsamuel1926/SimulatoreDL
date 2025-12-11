c5ac31a3d5e273b6ef1924c274e54ddcfe65a017
Daniele Lettera <infodanielplay@gmail.com>
2025-12-11 21:58:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,6 @@ tipo = option_menu(
         border-radius: 12px;
         padding: 20px;
         box-shadow: 0px 4px 12px rgba(0,0,0,0.1);
-}
         "nav-link": {"font-size": "18px", "color": "#005f91"},
         "nav-link:hover": {"background-color": "#0077b6"},
         "nav-link-selected": {"background-color": "#00BFFF", "color": "white"},
@@ -213,6 +212,7 @@ if st.button("Calcola Bolletta", key="calc"):
     except Exception as e:
 
         st.error(f"Errore nel calcolo: {e}")
+
 
 
 
