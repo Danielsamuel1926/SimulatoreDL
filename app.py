46d2229daeacde43b4259088cd77f91aafb9721c
Daniele Lettera <infodanielplay@gmail.com>
2025-12-11 22:10:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ body {
 # ==============================
 st.markdown("""
 <div class="header-container">
-    <span style="font-size:32px; font-weight:bold; color:#fff;">Simulatore Luce & Gas DL💡🔥</span>
+    <span style="font-size:32px; font-weight:bold; color:#fff;">Simulatore Luce & Gas DL</span>
 </div>
 """, unsafe_allow_html=True)
 
@@ -63,7 +63,7 @@ st.markdown("""
 # ==============================
 tipo = option_menu(
     menu_title="Seleziona Fornitura",
-    options=["Luce", "Gas"],
+    options=["Luce💡", "Gas🔥"],
     icons=["bolt", "fire"],
     menu_icon="solar-panel",
     default_index=0,
@@ -207,6 +207,7 @@ if st.button("Calcola Bolletta", key="calc"):
     except Exception as e:
 
         st.error(f"Errore nel calcolo: {e}")
+
 
 
 
