b92765a1db8bd531dc95488578dc1aca525dac4c
Daniele Lettera <infodanielplay@gmail.com>
2025-12-12 13:01:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -197,7 +197,7 @@ if st.button("Calcola Bolletta", key="calc"):
         #print(df.to_string(index=False))
         st.subheader("📊 Scontrino Bolletta DL CEI")
         #st.dataframe(df, column_config={"A": None})
-        st.table(df.to_dict(orient="records"))
+        #st.table(df.to_dict(orient="records"))
         st.markdown(f"### 💰 Totale: **{totale:.2f} €**")
                 
         diff = fatt_attuale - totale
@@ -210,6 +210,7 @@ if st.button("Calcola Bolletta", key="calc"):
 
     except Exception as e:
         st.error(f"Errore nel calcolo: {e}")
+
 
 
 
