8919a12213c69cbe01c4b95b585a3eb040b4dc40
Daniele Lettera <infodanielplay@gmail.com>
2025-12-12 16:45:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -188,13 +188,13 @@ if st.button("Calcola Bolletta", key="calc"):
             ]
             totale += materia+sp_rete+oneri+iva+comm_tot
 
-        for voce, val in [
-          ("Bonus Sociale", bonus),
-          ("Ricalcoli", ricalcoli),
-          ("Altre Partite", altre),
-          ("Canone TV", canone_tv)
-          ]:
-        if val > 0:
+            for voce, val in [
+    ("Bonus Sociale", bonus),
+    ("Ricalcoli", ricalcoli),
+    ("Altre Partite", altre),
+    ("Canone TV", canone_tv)
+]:
+    if val > 0:
         righe.append({"Voce": voce, "Importo (€)": f"{val:.2f}"})
         totale += val
 
